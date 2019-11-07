de373e7ef93d3bf0b8bebfbb1b4656b9ddc4d75c
borntorun33127 <35940115+borntorun33127@users.noreply.github.com>
2019-11-07 11:43:12 +0100

    Add files via upload
    Ajout nouvelle exception

---
+++ ProduitGratuitException.java
@@ -0,0 +1,17 @@
+package com.ecommerce.microcommerce.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProduitGratuitException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -4352937033973636989L;
+
+	public ProduitGratuitException(String s) {
+        super(s);
+    }
+}
